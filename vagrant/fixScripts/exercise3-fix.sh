33ce2a3ab2c8dc4a07f746305ef0c80afd65c9fe
zcohen331 <37035791+zcohen331@users.noreply.github.com>
2019-01-22 15:31:30 +0200

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #add fix to exercise3 here
-sudo sed -i -e 's/allow from none/allow from all/g' /etc/apache2/sites-available/default
-sudo /etc/init.d/apache2 start
+sed -i -e 's/allow from none/allow from all/g' /etc/apache2/sites-available/default
+/etc/init.d/apache2 start
 
